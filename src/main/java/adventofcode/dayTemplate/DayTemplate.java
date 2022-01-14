25cad985229962316930c2428983d4fbf1f01854
Jonas Fredriksson <jonas.fredriksson94@gmail.com>
2022-01-14 22:59:54 +0100

    Initial commit, restarting AoC 2017

---
+++ src/main/java/adventofcode/dayTemplate/DayTemplate.java
@@ -0,0 +1,23 @@
+package adventofcode.dayTemplate;
+
+import adventofcode.utils.FileHandler;
+
+import java.util.List;
+
+public class DayTemplate {
+
+    public long part1(String inputFile) {
+        List<String> lines = FileHandler.readFileIntoList(inputFile);
+
+
+        return 0;
+    }
+
+    public long part2(String inputFile) {
+        List<String> lines = FileHandler.readFileIntoList(inputFile);
+
+
+        return 0;
+    }
+
+}
